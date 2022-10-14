023511f88fd74b18cd4ebb624d6efcb34b52eb13
x4dr <x4dr@hotmail.com>
2022-10-14 17:54:43 +0200

    fix logic inversion

--- gamepack/MDPack.py
+++ gamepack/MDPack.py
@@ -109,7 +109,7 @@ class MDObj:
             log.info(msg)
 
         for subtable in self.tables:
-            skiprow = 1 if not not headers else 0
+            skiprow = 0 if not not headers else 1
             for row in subtable[skiprow:]:
                 if not row:
                     continue
